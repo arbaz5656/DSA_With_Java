9370be89071b03b6fb905f4a5037eb261a299611
arbaz5656 <139984287+arbaz5656@users.noreply.github.com>
2023-07-21 16:26:59 +0530

    Add files via upload
    Flowchart Questions

---
+++ Flowchart.java
@@ -0,0 +1,160 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.*;
+//public class Flowchart {
+//
+////    public static void add(int a,int b){
+////        int sum=a+b;
+////        System.out.println(sum);
+////    }
+//    public static void main(String[] args){
+////        add(2,4);
+//
+//
+//
+//// Find maximum of 3 Numbers
+//        int a=40;
+//        int b=5;
+//        int c=70;
+//
+//        if (a>b) {
+//            if (a > c) {
+//                System.out.println(a);
+//            } else {
+//                System.out.println(c);
+//            }
+//        }
+//        else  {
+//            if (b>c) {
+//                System.out.println(b);
+//            }
+//            else
+//            {
+//                System.out.println(c);
+//            }
+//        }
+//
+//
+//
+//
+//
+//    }
+//}
+
+
+
+//public class Flowchart {
+//    public static double simpleinterst(){
+//        Scanner v1=new Scanner(System.in);
+//        int p=v1.nextInt(); //50,000
+//        int r=v1.nextInt(); //8
+//        int t=v1.nextInt(); //1
+//        float si=p*r*t/100;
+//        return si;
+//
+//
+//    }
+//    public static void main(String[] args){
+//        System.out.println(simpleinterst());
+//
+//    }
+//}
+
+//
+//public class Flowchart {
+//    public static void main(String[] args) {
+//        Scanner v1=new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int num=v1.nextInt();
+//          int i = 2;
+//          boolean flag = false;
+//          while (i <= num / 2) {
+//              // condition for nonprime number
+//              if (num % i == 0) {
+//                  flag = true;
+//                  break;
+//               }
+//               ++i;
+//            }
+//          if (!flag)
+//              System.out.println(num + " is a prime number.");
+//          else
+//              System.out.println(num + " is not a prime number.");
+//        }
+//    }
+//
+
+//sum of N natural number
+//public class Flowchart {
+//    public static void main(String[] args){
+//        int n=10,sum=0;
+//        for (int i=1;i<=n;i++){
+//            sum=sum+i;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//Calculate Area of circle
+//public class Flowchart {
+//    public static double Araofcircle(int r){
+//        double area=3.14*r*r;
+//        return area;
+//    }
+//    public static void main(String[] args){
+//        Scanner v1=new Scanner(System.in);
+//        System.out.println("Enter the Radius r:");
+//        int r=v1.nextInt();
+//        System.out.println(Araofcircle(r));
+//
+//    }
+//}
+
+//find the greatestes number from 2.
+//public class Flowchart {
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the Number A:");
+//        int a=sc.nextInt();
+//        System.out.println("Enter the Number B:");
+//        int b=sc.nextInt();
+//        if (a>b){
+//            System.out.println(a+" Number A is Greter.");
+//        }
+//        else {
+//            System.out.println(b+" Number B is Greter.");
+//        }
+//    }
+//
+
+//print Even and Odd between 9 to 100
+//public class Flowchart {
+//    public static void main(String[] args){
+//        for (int i=9;i<=100;i++){
+//            if (i%2==0){
+//                System.out.println("Even: "+i);
+//            }
+//            else {
+//                System.out.println("Odd: "+i);
+//            }
+//        }
+//    }
+//}
+
+
+//Print * programme
+//public class Flowchart {
+//    public static void main(String[] args){
+//        for (int i=4;i>=1;i--){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+public class Flowchart {
+
+}

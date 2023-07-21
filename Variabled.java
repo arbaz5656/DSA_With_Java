743d2b933b0a4b1798848545552de7180799d23f
arbaz5656 <139984287+arbaz5656@users.noreply.github.com>
2023-07-22 00:31:24 +0530

    Add files via upload

---
+++ Variabled.java
@@ -0,0 +1,147 @@
+//public class Variabled {
+//    public static void main(String[] args){
+//        String str="Hello Arbaz";
+//        int integer=2;
+//        System.out.println(str);
+//        str="Shaikh Arbaz";
+//        integer=10;
+//        System.out.println(str+" "+integer);
+//
+//    }
+//}
+
+//sum A and B
+//public class Variabled {
+//    public static void main(String[] args){
+//        int a=5;
+//        int b=10;
+//        int sum=a+b;
+//        System.out.println(sum);
+//
+//    }
+//}
+
+//input
+//import java.util.Scanner;
+//public class Variabled {
+//    public static void main(String[]argd){
+//        Scanner sc= new Scanner(System.in);
+////        String op=sc.next();
+////        String op=sc.nextLine();
+//        int op=sc.nextInt();
+//        System.out.println(op);
+//
+//
+//    }
+
+
+//import java.util.Scanner;
+////sum of a & b using input
+//public class Variabled {
+//    public static void main(String[] args){
+//        Scanner one=new Scanner(System.in);
+//        System.out.println("Enter the 1st number:");
+//        int a=one.nextInt();
+//        System.out.println("Enter the 2nd number:");
+//        int b=one.nextInt();
+//        int sum=a+b;
+//        System.out.println("Sum of total :"+sum);
+//
+//}
+//}
+
+//
+//import java.util.Scanner;
+////area of circle
+//public class Variabled {
+//    public static void main(String[] args){
+//        Scanner obj=new Scanner(System.in);
+//        System.out.println("Enter the radius: ");
+//        double radius=obj.nextDouble();
+//        double area=3.14*radius*radius;
+//        System.out.println(area);
+//    }
+//}
+//
+
+//type conversion
+//import java.util.Scanner;
+//public class Variabled {
+//    public static void main(String[] args){
+//        Scanner obj=new Scanner(System.in);
+////        int var=obj.nextFloat();
+//        float var=obj.nextInt();
+//        System.out.println(var);
+//    }
+//}
+//
+////type casting
+//public class Variabled {
+//    public static void main(String[] args) {
+//        float a=4;
+//        int b=(int) a;
+//        System.out.println(b);
+//
+//        char c='b';
+//        int d=c;
+//        System.out.println(d);
+//
+//    }
+//
+//}
+
+
+
+////Q.1
+//public class Variabled {
+//    public static void main(String[] args){
+//        int one=5;
+//        int two=5;
+//        int three=5;
+//        int avg=(one+two+three)/3;
+//        System.out.println(avg);
+//    }
+//}
+
+import java.util.Scanner;
+
+//Q.2
+//public class Variabled {
+//    public static void main(String []args){
+//        Scanner obj=new Scanner(System.in);
+//        int var=obj.nextInt();
+//        int square=var*var;
+//        System.out.println(square);
+//    }
+//}
+
+//Q.3
+
+//public class Variabled {
+//    public static void main(String[] args){
+//        Scanner obj=new Scanner(System.in);
+//        System.out.println("Enter the pencil price: ");
+//        float pencil=obj.nextFloat();
+//        System.out.println("Enter the pen price: ");
+//        float pen=obj.nextFloat();
+//        System.out.println("Enter the eraser price: ");
+//        float eraser=obj.nextFloat();
+//        float Toatl=pencil+pen+eraser;
+//        System.out.println("Total bill: "+Toatl );
+//        float gst=Total+(0.18f*Toatl);
+//        System.out.println("Total bill with gst:"+gst);
+//
+//    }
+//}
+
+//Q.5
+//public class Variabled {
+//    public static void main(String[] args){
+//        int $=25;
+//        System.out.println($);
+//    }
+//}
+
+public class Variabled {
+
+}
